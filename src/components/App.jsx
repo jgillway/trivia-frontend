bc346d592d7eff19e662bbb5c80de361c4e7ac92
jgillway <jgillway@gmail.com>
2021-12-14 22:11:11 -0500

    basic flip

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import './styles/App.css';
+
+function App() {
+  function handleCardClick(e) {
+    e.parentElement.classList.toggle('is-flipped')
+  }
+
+  return (
+    <div className='container'>
+      <div className='card' onClick={(e) => handleCardClick(e.target)}>
+        <div className='card__face card__face--front'>test</div>
+        <div className='card__face card__face--back'>test back</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
