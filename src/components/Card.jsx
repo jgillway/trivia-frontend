f7afacf7585106540e65e8479caeb868981980c8
jgillway <jgillway@gmail.com>
2022-01-12 20:40:22 -0500

    Style: -Added height to footer Bug: -Refactored card component to fix flipping errors with refresh

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,31 +1,31 @@
+import React from 'react';
 import './styles/Card.css';
 
-function Card() {
-  const cards = document.getElementsByClassName('card');
-  for (var i = 0; i < cards.length; i++) {
-      cards[i].addEventListener('click', function(){
-          this.classList.toggle('is-flipped');
-      })
-  };
-
-  return (
-    <div className='container'>
-      <div className='card'>
-        <div className='card__face card__face--front'>
-          <div className='titleFront'>
-            category
+class Card extends React.Component {
+  state = {
+    isFlipped: false
+  }
+  render() {
+    const { isFlipped } = this.state;
+    return (
+      <div className='container'>
+        <div className={`card ${this.state.isFlipped ? ' is-flipped' : ''}`} onClick={() => this.setState({isFlipped: !isFlipped})}>
+          <div className='card__face card__face--front'>
+            <div className='titleFront'>
+              category
+            </div>
+            <p>Question</p>
           </div>
-          <p>Question</p>
-        </div>
-        <div className='card__face card__face--back'>
-          <div className='titleBack'>
-            question
+          <div className='card__face card__face--back'>
+            <div className='titleBack'>
+              question
+            </div>
+            <p>Answer</p>
           </div>
-          <p>Answer</p>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Card;
